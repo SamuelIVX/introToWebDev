7c5ab6c521527e2669aa79517e046dc2ce0e55a3
Samuel <samuel05.hb@gmail.com>
2026-01-02 00:58:49 -0500

    README: slight modification for cleanliness.

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Intro To Web Development and the Internet
 
-## Labs from this course:
-[Lab2](https://samuelivx.github.io/introToWebDev/)
+A collection of labs and projects from an introductory web development course covering HTML, CSS, and JavaScript fundamentals.
 
-[Lab3](https://samuelivx.github.io/introToWebDev/Lab3.html)
+## Labs
 
-[Lab4](https://samuelivx.github.io/introToWebDev/Lab4.html)
-
-[Lab5](https://samuelivx.github.io/introToWebDev/Lab5.html)
-
-[Lab6](https://samuelivx.github.io/introToWebDev/Lab6.html)
-
-[Final Project](https://samuelivx.github.io/introToWebDev/finalProject/index.html)
+- [Lab 2](https://samuelivx.github.io/introToWebDev/)
+- [Lab 3](https://samuelivx.github.io/introToWebDev/Lab3.html)
+- [Lab 4](https://samuelivx.github.io/introToWebDev/Lab4.html)
+- [Lab 5](https://samuelivx.github.io/introToWebDev/Lab5.html)
+- [Lab 6](https://samuelivx.github.io/introToWebDev/Lab6.html)
+- [Final Project](https://samuelivx.github.io/introToWebDev/finalProject/index.html)
--- finalProject/script.js
+++ finalProject/script.js
@@ -209,7 +209,6 @@ function generateCharts(data) {
 
 async function fetchNewsData(filterType){
     //   const url = `https://finnhub.io/api/v1/news?category=${filterType}&token=${api_key}`;
-    
     //   // Create and return a Promise
     //   const newsPromise = new Promise((resolve, reject) => {
     //     fetch(url)
@@ -248,14 +247,13 @@ async function fetchNewsData(filterType){
     //         document.getElementById("api-content").innerHTML = "Failed to load news.";
     //         }
     //     );
-    
-        try{
+
+    try{
         const url = `https://finnhub.io/api/v1/news?category=${filterType}&token=${api_key}`;
         const response = await fetch(url);
         const data = await response.json();
-
         document.getElementById("news-para").innerHTML = "<p></p>"; // After a filter is applied, the message about "Applying a filter" should be removed so I will will reset the p-tag
-        
+
         let output = "";
         data.slice(0, 30).forEach(key => {
             output += `
@@ -268,8 +266,8 @@ async function fetchNewsData(filterType){
             `;
         });
         document.getElementById("api-content").innerHTML = output;
-        } catch (e) {
-            let errorOutput = `<p style="color:red;color:red;font-weight:bold">Failed to fetch news articles...</p>`
-            document.getElementById("news-para").innerHTML = errorOutput;
-        }
+    } catch (e) {
+        let errorOutput = `<p style="color:red;color:red;font-weight:bold">Failed to fetch news articles...</p>`
+        document.getElementById("news-para").innerHTML = errorOutput;
     }
+}
